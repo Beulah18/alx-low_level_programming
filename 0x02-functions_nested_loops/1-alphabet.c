e1adc28178d7c8dcf76f572df62a748090957556
Beulah <igboanugobeulah18@gmail.com>
2022-09-20 23:04:10 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point
+ *
+ * Return:nothing
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 97; alpha <= 122; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
