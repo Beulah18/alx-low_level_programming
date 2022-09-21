ca786f35ecd5a0a343d2c4f27d0d47ad09652ebb
Beulah <igboanugobeulah18@gmail.com>
2022-09-21 08:06:19 -0700

    C is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _islower - Entry point
+ *
+ * Return:1 else 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
